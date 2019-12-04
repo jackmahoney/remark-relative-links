df1c201759175883cd9ee9c46f879a75cfdc021e
Jack Mahoney <jackmahoney212@gmail.com>
2019-12-04 12:43:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const visit = require('unist-util-visit');
 
 module.exports = function (options) {
-  if (!options || !options.domainRegex) {
-    throw Error('Missing required "domainRegex" option');
+  if (!options || !options.domainRegex || !options.replacement) {
+    throw Error('Missing required "domainRegex" or "replacement" option');
   }
 
   function visitor(node) {
     if (options.domainRegex.test(node.url)) {
-      node.url = node.url.replace(options.domainRegex, '/');
+      node.url = node.url.replace(options.domainRegex, options.replacement);
     }
   }
 
